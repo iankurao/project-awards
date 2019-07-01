935622cf7ecba152742d6854f1dbd71ad97641fe
iankurao <kuraoian@gmail.com>
2019-07-01 15:55:30 +0300

    deployment to heroku

--- Awards/settings.py
+++ Awards/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import django_heroku
+import dj_database_url
+from decouple import config,Csv
+MODE=config('MODE',default='dev')
+SECRET_KEY=config("SECRET_KEY")
+DEBUG=config("DEBUG",default=False,cast=bool)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,12 +26,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^ir_t5ka9r=9ciph_+yxdrhcz!akav_%ypg0xu(v_438bwj6=&'
+# SECRET_KEY = '^ir_t5ka9r=9ciph_+yxdrhcz!akav_%ypg0xu(v_438bwj6=&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+LOGIN_REDIRECT_URL='/'
+REGISTRATION_OPEN=True
 
 
 # Application definition
@@ -43,6 +51,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -75,14 +84,29 @@ WSGI_APPLICATION = 'Awards.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'awards',
-        'USER': 'ian',
-    'PASSWORD':'vionashina',
+if config('MODE')=='dev':
+    
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME':config('DB_NAME'),
+            'USER':config("DB_USER"),
+            'PASSWORD':config("DB_PASSWORD"),
+            'HOST':config("DB_HOST")
+        }
     }
-}
+
+else:
+    DATABASES={
+    'default':dj_database_url.config(
+    default=config('DATABASE_URL')
+    )
+    }
+
+db_from_evn=dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_evn)
+ALLOWED_HOSTS=config('ALLOWED_HOSTS',cast=Csv)
+
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
@@ -119,10 +143,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATIC_ROOT='static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+django_heroku.settings(locals())
