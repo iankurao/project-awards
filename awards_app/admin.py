cf37a4aede135239c6741e1c25ee088fd2848ae4
iankurao <kuraoian@gmail.com>
2019-07-01 11:34:42 +0300

    register our models

--- awards_app/admin.py
+++ awards_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Profile,Projects,Comments
 # Register your models here.
+admin.site.register(Projects)
+admin.site.register(Profile)
+admin.site.register(Comments)
