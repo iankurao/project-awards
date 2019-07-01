f1141c94bcd4830fc8076ddd83a76276383f48eb
iankurao <kuraoian@gmail.com>
2019-07-01 11:09:11 +0300

    create a view function for home page

--- awards_app/views.py
+++ awards_app/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
+def index(request):
+    
+    try:
+        projects=Projects.objects.all()
+    except Exception as e:
+        raise  Http404()
+    return render(request,'index.html',{"projects":projects})
