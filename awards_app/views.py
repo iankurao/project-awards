254b158be5a8330234b89238a7320df323fcdee9
iankurao <kuraoian@gmail.com>
2019-07-01 12:12:39 +0300

    import our forms and models to our views file

--- awards_app/views.py
+++ awards_app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render,redirect
-
+from .forms import PostForm,RateForm,ReviewForm,UpdateForm
+from .models import Projects,Rates,Comments,Profile
+from django.http import HttpResponse,Http404,HttpResponseRedirect,JsonResponse
+from django.contrib.auth.models import User
 # Create your views here.
 def index(request):
     
