22d7c8951cdcf28c47882c9225000cf38a03fc04
iankurao <kuraoian@gmail.com>
2019-07-01 11:34:12 +0300

    update our models

--- awards_app/models.py
+++ awards_app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Projects(models.Model):
+    name=models.CharField(max_length=30)
+    image=models.ImageField(upload_to='projects/')
+    design=models.IntegerField(default=0)
+    usability=models.IntegerField(default=0)
+    content=models.IntegerField(default=0)
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    description=models.TextField(max_length=320)
+    link=models.URLField(max_length=60)
+    date=models.DateField(auto_now=True)
+    screen1=models.ImageField(upload_to='screenshot/',blank=True)
+    screen2=models.ImageField(upload_to='screenshot/',blank=True)
+
+class Profile(models.Model):
+    profile=models.ImageField(upload_to='profile/')
+    bio=models.CharField(max_length=60)
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    phone=models.IntegerField()
+
+class Comments(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    comment=models.TextField(max_length=200)
+    pro_id=models.IntegerField(default=0)
+
+
